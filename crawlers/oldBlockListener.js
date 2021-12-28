0b111391846849a533dfce63848dae65b82b65fc
Sergey Spitsyn <sswebcoder@gmail.com>
2021-12-28 14:59:45 +0300

    Jira issue [NFTPAR-1026].

--- crawlers/oldBlockListener.js
+++ crawlers/oldBlockListener.js
@@ -13,148 +13,94 @@ const eventsDB = require('../lib/eventsDB.js');
 const { QueryTypes } = require('sequelize');
 
 const loggerOptions = {
-  crawler: `oldBlockListenr`,
+  crawler: `oldBlockListener`,
 };
 
-async function getBlock({
-  api,
+async function syncBlock({
   sequelize,
+  bridgeAPI,
   blockNumber,
-  loggerOptions,
   logger,
+  loggerOptions,
 }) {
-  const bridgeAPI = new BridgeAPI(api).bridgeAPI;
+  logger.info(`Syncing block ${blockNumber}`);
 
-  const blockInfo = await blockData.get({
-    blockNumber,
-    bridgeAPI,
-  });
+  const blockInfo = await blockData.get({ bridgeAPI, blockNumber });
+  const events = await eventsData.get({ bridgeAPI, blockHash: blockInfo.blockHash });
+  const timestampMs = await bridgeAPI.api.query.timestamp.now.at(blockInfo.blockHash);
+  const sessionLength = (
+    bridgeAPI.api.consts?.babe?.epochDuration || 0
+  ).toString();
 
-  const getBlockDB = await blockDB.get({
+  await blockDB.add({
     blockNumber,
+    block: Object.assign(blockInfo, events, { timestampMs, sessionLength }),
     sequelize,
   });
 
+  await storeExtrinsics(
+    sequelize,
+    blockNumber,
+    blockInfo.extrinsics,
+    events.blockEvents,
+    timestampMs,
+    loggerOptions
+  );
 
-  if (getBlockDB.length > 0) {
-
-    const block = getBlockDB[0];
-    const check = blockData.check({
-      sourceBlock: block,
+  await eventsData.events(events.blockEvents, async (record, index) => {
+    const res = await eventsDB.get({
       blockNumber,
-      blockInfo
+      index,
+      sequelize,
     });
 
-    if (!check) {
-      logger.default(
-        `Detected chain reorganizaition at block #${blockNumber}, updating authot, author name, hash ans state root`
+    const preEvent = Object.assign(
+      {
+        block_number: blockNumber,
+        event_index: index,
+        timestamp: Math.floor(timestampMs / 1000),
+      },
+      eventsData.parseRecord(record)
+    );
+
+    if (!res) {
+      await eventsDB.add({ event: preEvent, sequelize });
+
+      logger.info(
+        `Added event #${blockNumber}-${index} ${preEvent.section} ➡ ${preEvent.method}`
       );
-      await blockDB.modify({ blockNumber, blockInfo, sequelize });
     }
-
-  } else {
-    logger.info(
-      `Adding block #${blockNumber} (${shortHash(
-        blockInfo.blockHash.toString()
-      )})`
-    );
-
-    const events = await eventsData.get({
-      bridgeAPI,
-      blockHash: blockInfo.blockHash,
-    });
-
-    const timestampMs = await bridgeAPI.api.query.timestamp.now.at(
-      blockInfo.blockHash
-    );
-    const sessionLength = (
-      bridgeAPI.api.consts?.babe?.epochDuration || 0
-    ).toString();
-
-    await blockDB.add({
-      blockNumber,
-      block: Object.assign(blockInfo, events, { timestampMs, sessionLength }),
-      sequelize,
-    });
-
-    await storeExtrinsics(
-      sequelize,
-      blockNumber,
-      blockInfo.extrinsics,
-      events.blockEvents,
-      timestampMs,
-      loggerOptions
-    );
-
-    await eventsData.events(events.blockEvents, async (record, index) => {
-      const res = await eventsDB.get({
-        blockNumber,
-        index,
-        sequelize,
-      });
-
-      const preEvent = Object.assign(
-        {
-          block_number: blockNumber,
-          event_index: index,
-          timestamp: Math.floor(timestampMs / 1000),
-        },
-        eventsData.parseRecord(record)
-      );
-
-      if (!res) {
-        await eventsDB.add({ event: preEvent, sequelize });
-
-        logger.info(
-          `Added event #${blockNumber}-${index} ${preEvent.section} ➡ ${preEvent.method}`
-        );
-      }
-    });
-    updateTotals(sequelize, loggerOptions);
-  }
-}
-
-async function getOldBlock({
-  firstBlock,
-  lastBlock,
-  api,
-  sequelize,
-  logger,
-  loggerOptions,
-}) {
-  const range = genArrayRange(firstBlock, lastBlock);
-
-  for (const item of range) {
-    await getBlock({
-      api,
-      sequelize,
-      blockNumber: item,
-      loggerOptions,
-      logger,
-    });
-  }
-}
-
-async function getLastBlock(api) {
-  const bridgeAPI = new BridgeAPI(api).bridgeAPI;
-  const result = await blockData.last(bridgeAPI);
-  return result;
+  });
+  updateTotals(sequelize, loggerOptions);
 }
 
 async function start({ api, sequelize, config }) {
   const logger = new Logger();
 
-  logger.start(`Startinf block listner for old blocks...`);
+  logger.start(`Starting block listener for old blocks...`);
 
-  const blockNumber = await getLastBlock(api);
+  const blocksGaps = await blockDB.getBlocksGaps({ sequelize });
 
-  await getOldBlock({
-    firstBlock: 0,
-    lastBlock: blockNumber,
-    api,
-    sequelize,
-    logger,
-    loggerOptions,
+  logger.info(`${blocksGaps.length} gaps founded`);
+  logger.info('blocksGaps: ', blocksGaps);
+
+  const bridgeAPI = new BridgeAPI(api).bridgeAPI;
+
+  blocksGaps.forEach(async (blockGap) => {
+    logger.info(`Filling gap between block number ${blockGap.gapStart}-${blockGap.gapEnd}`);
+
+    const gapStart = Number(blockGap.gapStart);
+    const gapEnd = Number(blockGap.gapEnd);
+
+    for (let blockNumber = gapStart + 1; blockNumber < gapEnd; blockNumber++) {
+      await syncBlock({
+        sequelize,
+        bridgeAPI,
+        blockNumber,
+        logger,
+        loggerOptions,
+      });
+    }
   });
 }
 
