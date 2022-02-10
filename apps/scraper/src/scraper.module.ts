b939f3e08623117ef75a91a0345383ad1d71248a
Sergey Spitsyn <sswebcoder@gmail.com>
2022-02-10 14:14:16 +0300

    Added config to Scrapper module.

--- apps/scraper/src/scraper.module.ts
+++ apps/scraper/src/scraper.module.ts
@@ -1,9 +1,12 @@
+import typeormConfig from '@common/typeorm.config';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScraperController } from './scraper.controller';
 import { ScraperService } from './scraper.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(typeormConfig)],
   controllers: [ScraperController],
   providers: [ScraperService],
 })
