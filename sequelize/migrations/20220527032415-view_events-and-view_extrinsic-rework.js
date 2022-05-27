2f67b7d1f9a20a79e526172e67a63d6b9219e0ac
Sergey Chernetsky <chernetsky@gmail.com>
2022-05-27 12:51:22 +0800

    Views rework migration

---
+++ sequelize/migrations/20220527032415-view_events-and-view_extrinsic-rework.js
@@ -0,0 +1,115 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.sequelize.query('DROP VIEW view_extrinsic;', { transaction });
+      
+      await queryInterface.sequelize.query('DROP VIEW view_events;', { transaction });
+
+      // todo: Эта вьюха теперь вообще нужна?
+      await queryInterface.sequelize.query(
+        `
+        CREATE OR REPLACE VIEW public.view_events
+        AS SELECT 
+          block_index,
+          block_number,
+          amount,
+          fee
+        FROM extrinsic;
+        `,
+        { transaction },
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        CREATE OR REPLACE VIEW public.view_extrinsic
+        AS SELECT 
+          block_index,
+          block_number,
+          signer AS from_owner,
+          signer_normalized AS from_owner_normalized,
+          to_owner,
+          to_owner_normalized,
+          hash,
+          success,
+          timestamp,
+          method,
+          section,
+          amount,
+          fee
+        FROM extrinsic
+        WHERE method = ANY (ARRAY['transfer', 'transferAll', 'transferKeepAlive', 'vestedTransfer']);;
+        `,
+        { transaction },
+      );
+      await transaction.commit();
+    } catch (err) {
+      console.error(err);
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.sequelize.query('DROP VIEW view_extrinsic;', { transaction });
+
+      await queryInterface.sequelize.query('DROP VIEW view_events;', { transaction });
+
+      await queryInterface.sequelize.query(
+        `
+        CREATE OR REPLACE VIEW public.view_events
+        AS SELECT view_events.block_index,
+            view_events.block_number,
+            sum(view_events.amount::double precision)::text AS amount,
+            sum(view_events.fee::double precision)::text AS fee
+          FROM ( SELECT event.block_index,
+                    event.block_number,
+                        CASE
+                            WHEN event.method = 'Transfer'::text THEN event.amount
+                            ELSE NULL::text
+                        END AS amount,
+                        CASE
+                            WHEN event.method = 'Deposit'::text THEN event.amount
+                            ELSE NULL::text
+                        END AS fee
+                  FROM event
+                  WHERE event.phase <> 'Initialization'::text AND event.section = 'balances'::text AND (event.method = ANY (ARRAY['Transfer'::text, 'Deposit'::text]))) view_events
+          GROUP BY view_events.block_index, view_events.block_number;
+        `,
+        { transaction },
+      );
+      
+      await queryInterface.sequelize.query(
+        `
+        CREATE OR REPLACE VIEW public.view_extrinsic
+        AS SELECT extrinsic.block_index,
+            extrinsic.block_number,
+            extrinsic.signer AS from_owner,
+            extrinsic.signer_normalized AS from_owner_normalized,
+            extrinsic.to_owner,
+            extrinsic.to_owner_normalized,
+            extrinsic.hash,
+            extrinsic.success,
+            extrinsic."timestamp",
+            extrinsic.method,
+            extrinsic.section,
+            ev.amount,
+            ev.fee
+          FROM extrinsic
+            LEFT JOIN view_events ev ON ev.block_index = extrinsic.block_index::text
+          WHERE extrinsic.method = ANY (ARRAY['transfer'::text, 'transferAll'::text, 'transferKeepAlive'::text, 'vestedTransfer'::text]);
+        `,
+        { transaction },
+      );
+      await transaction.commit();
+    } catch (err) {
+      console.error(err);
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
