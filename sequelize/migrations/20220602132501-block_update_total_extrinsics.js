611c9b73eaa643489f73c77e67885df60f10b5dc
Sergey Spitsyn <sswebcoder@gmail.com>
2022-06-02 17:05:01 +0300

    Optimize getting stats for blocks.

---
+++ sequelize/migrations/20220602132501-block_update_total_extrinsics.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.sequelize.query(
+        `update block b
+          set total_extrinsics = (select count(*) from extrinsic e where e.block_number = b.block_number);
+        `,
+        { transaction },
+      );
+
+      await queryInterface.changeColumn(
+        'block',
+        'total_extrinsics',
+        {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        {
+          transaction,
+        },
+      );
+
+      await transaction.commit();
+    } catch (err) {
+      console.error(err);
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  async down (queryInterface, Sequelize) {},
+};
