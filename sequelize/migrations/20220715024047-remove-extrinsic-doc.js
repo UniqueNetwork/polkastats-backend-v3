598fab3cb44c1ebb17bc47fa4d33f2af2841b25e
Sergey Chernetsky <chernetsky@gmail.com>
2022-07-15 11:02:41 +0800

    Minor migration fix

--- sequelize/migrations/20220715024047-remove-extrinsic-doc.js
+++ sequelize/migrations/20220715024047-remove-extrinsic-doc.js
@@ -8,7 +8,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.addColumn('extrinsic', 'doc', {
       type: Sequelize.DataTypes.TEXT,
-      allowNull: false,
+      allowNull: true,
     });
   },
 };
