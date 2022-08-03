911d1c13637b3419aeb95459847e58e3edd4aa72
Sergey Chernetsky <chernetsky@gmail.com>
2022-08-03 13:19:10 +0800

    Added migration

---
+++ sequelize/migrations/20220802102941-collection-properties-and-attributes-schema.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.addColumn('collections', 'properties', {
+        type: Sequelize.DataTypes.JSONB,
+        defaultValue: {}  
+      }),
+      queryInterface.addColumn('collections', 'attributes_schema', {
+        type: Sequelize.DataTypes.JSONB,
+        defaultValue: {}  
+      }),
+    ]) 
+  },
+
+  async down (queryInterface, Sequelize) {
+    return Promise.all([
+      await queryInterface.removeColumn('collections', 'properties'),
+      await queryInterface.removeColumn('collections', 'attributes_schema'),
+    ]);
+  }
+};
